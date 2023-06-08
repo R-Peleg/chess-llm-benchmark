47ba549a76be188eab6378880ca3a16e68b34e57
Reuven Peleg <reuven.peleg@orcam.com>
2023-06-08 10:51:47 +0300

    Generate legal and illegal moves

--- src/generate_benchmark.py
+++ src/generate_benchmark.py
@@ -2,6 +2,7 @@ import os
 import requests
 import chess
 import chess.pgn
+import random
 
 ARTIFACTS_DIR = os.path.join(os.path.dirname(__file__), os.pardir, 'artifacts')
 PGN_FILE = os.path.join(ARTIFACTS_DIR, 'games.pgn')
@@ -16,14 +17,41 @@ def main():
         with open(PGN_FILE, 'w') as f:
             f.write(response.content.decode('utf-8'))
     print('Reading games')
+    first_moves_list = []
+    legal_moves = []
+    illegal_moves = []
     with open(PGN_FILE, 'r') as pgn_f:
-        game = chess.pgn.read_game(pgn_f)
-        while game:
-            for move in game.mainline_moves():
-                print(move)
-            break
+        while True:
             game = chess.pgn.read_game(pgn_f)
-
+            if game is None:
+                break
+            board = game.board()
+            moves = []
+            illegal_move = None
+            for move in game.mainline_moves():
+                san = board.san(move)
+                moves.append(san)
+                if len(moves) >= 5:
+                    # Try to find pseudo legal move
+                    for m in board.pseudo_legal_moves:
+                        if not board.is_legal(m):
+                            illegal_move = m
+                            break
+                    current_legal_moves = [board.san(m) for m in board.legal_moves]
+                    while illegal_move is None:
+                        m = random.choice(['K', 'Q', 'R', 'N', 'B', '']) + random.choice('abcdefgh') + random.choice('12345678')
+                        if m not in current_legal_moves:
+                            illegal_move = m
+                    break
+                board.push(move)
+            moves_tuple = tuple(moves[:-1])
+            if moves_tuple not in first_moves_list:
+                first_moves_list.append(moves_tuple)
+                legal_moves.append(moves[-1])
+                illegal_moves.append(illegal_move)
+    print(f'Found {len(first_moves_list)} first moves sequences')
+    for moves, lm, ilm in zip(first_moves_list, legal_moves, illegal_moves):
+        print(moves, lm, ilm)
 
 
 
